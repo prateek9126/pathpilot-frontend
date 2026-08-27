32b5d1293ccdc5db2b6a6a617463ddbbedf17111
prateek kumar <prateek2222kumar@gmail.com>
2026-08-28 00:28:28 +0530

    Connect frontend to Render backend

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:8080/api';
-
+//const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 async function handleResponse(response) {
     if (!response.ok) {
         const errorText = await response.text();
